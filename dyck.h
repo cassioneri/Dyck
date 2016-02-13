7746352d43dd8eac8a573f42be9cae1d48966ca7
Cassio Neri <cassio.neri@gmail.com>
2016-02-13 19:02:02 +0000

    Fixed typos.

--- dyck.h
+++ dyck.h
@@ -1,12 +1,13 @@
 /*******************************************************************************
 
- For a discussion about this code see http://github.com/cassioneri/Dyck
-
  Copyright (C) 2014 Cassio Neri.
 
  This work is licensed under a Creative Commons Attribution-ShareAlike 4.0
  International License. See
  http://creativecommons.org/licenses/by-sa/4.0/
+ 
+ For a discussion about this code and latest version:
+ http://github.com/cassioneri/Dyck
  
 *******************************************************************************/
 
